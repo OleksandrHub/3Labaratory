002c6f9c45350e97e0f570bd55aa16df3728814c
Oleksandr <sasha.klishch1@gmail.com>
2025-02-16 12:40:48 +0200

    Create variant2.cpp

---
+++ variant2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    double C=2.1 , D=0.56 ;
+    double x ,y,result;
+    printf("x = "); 
+    scanf("%lf",&x);
+    printf("y = ");
+    scanf("%lf",&y);
+    if (x > y) result = exp(x) + (C*log10(pow(x,2)));
+    else result = pow(x*y , 1.0/3.0) + (D*y);
+     printf("%lf", result);
+    return 0;
+}
